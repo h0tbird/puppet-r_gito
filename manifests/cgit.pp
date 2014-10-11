486c6d391e29185fafd8f05f0ed0c8e87afdc845
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-11 21:00:36 +0200

    Back to git_home

--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -3,7 +3,7 @@ class r_gito::cgit inherits r_gito {
   if $cgit {
     class { '::cgit':
       version  => $cgit['version'],
-      git_user => $cgit['git_user'],
+      git_user => $cgit['git_home'],
     }
   }
 }
