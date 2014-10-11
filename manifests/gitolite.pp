2ac7e0c9a199df2451f92a0e07932dba1eb0de80
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-11 16:27:56 +0200

    Adding parameters to class declaration

--- manifests/gitolite.pp
+++ manifests/gitolite.pp
@@ -2,7 +2,10 @@ class r_gito::gitolite inherits r_gito {
 
   if $gitolite {
     class { '::gitolite':
-      version => $gitolite['version'],
+      version   => $gitolite['version'],
+      git_user  => $gitolite['git_user'],
+      git_admin => $gitolite['git_admin'],
+      ssh_key   => $gitolite['ssh_key'],
     }
   }
 }
