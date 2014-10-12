cf517adf1d2d700e49ded9195e93b84d85af8534
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-12 09:54:15 +0200

    Add purge_configs parameter

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -4,6 +4,7 @@ class r_gito::apache inherits r_gito {
     class { '::apache':
       default_mods        => $apache['default_mods'],
       default_confd_files => $apache['default_confd_files'],
+      purge_configs       => $apache['purge_configs'],
     }
   }
 }
